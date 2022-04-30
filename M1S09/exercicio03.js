7680c74768c3faca2dfba5dfc8914c8ba70b4fb9
Vitor Serrão <vitorwss@gmail.com>
2022-04-30 16:19:07 -0300

    ex03

---
+++ M1S09/exercicio03.js
@@ -0,0 +1,7 @@
+var aluno = {
+    nome: 'vitor',
+    matricula: 1244,
+    ativo: true,
+    cadeiras: ['mtm', 'fsc', 'bio', 'qmc']
+};
+console.log(aluno);
---
+++ M1S09/exercicio03.ts
@@ -0,0 +1,13 @@
+let aluno: {
+  nome: string;
+  matricula: number;
+  ativo: boolean;
+  cadeiras: string[];
+} = {
+  nome: 'vitor',
+  matricula: 1244,
+  ativo: true,
+  cadeiras: ['mtm', 'fsc', 'bio', 'qmc'],
+};
+
+console.log(aluno);
