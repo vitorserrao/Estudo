1baeb0309733c63b92e84586b8354cb33e51a150
Vitor Serrão <vitorwss@gmail.com>
2022-04-30 16:37:57 -0300

    ex4

---
+++ M1S09/exercicio04.js
@@ -0,0 +1,10 @@
+var Filme = /** @class */ (function () {
+    function Filme() {
+    }
+    return Filme;
+}());
+var nos = new Filme();
+nos.nome = 'Nós';
+nos.anoLancamento = 2015;
+nos.diretor = 'n sei';
+console.log(nos);
---
+++ M1S09/exercicio04.ts
@@ -0,0 +1,11 @@
+class Filme {
+  nome: string;
+  anoLancamento: number;
+  diretor: string;
+}
+
+const nos = new Filme();
+nos.nome = 'Nós';
+nos.anoLancamento = 2015;
+nos.diretor = 'n sei';
+console.log(nos);
